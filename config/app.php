db75b853da9d45fa23be5be99cd6738e79bc469a
Gold <709862717@qq.com>
2022-09-13 12:11:57 +0800

    fix：基础配置项

--- config/app.php
+++ config/app.php
@@ -174,8 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
-        SwooleTw\Http\LaravelServiceProvider::class
+        App\Providers\RouteServiceProvider::class
 
     ],
 
